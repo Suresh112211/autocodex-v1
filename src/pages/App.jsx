102d152784dda214983b21689b6a7bacad81cdda
Suresh <adanisuresh@gmail.com>
2025-07-17 21:13:05 +0530

    Update App.jsx
    Add for

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"; // make sure the path is correct
+// src/App.jsx
+import React from "react";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {/* Add other routes as needed */}
-      </Routes>
-    </Router>
+    <div>
+      <Login />
+    </div>
   );
 }
 
